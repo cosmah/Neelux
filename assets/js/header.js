632b3f2585cf8afd63c981ceb5fc104030b5e1fa
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-03-27 14:15:34 +0300

    worked on the menu

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,15 +1,15 @@
-const searchBtn = document.querySelector('.search-btn');
-const searchForm = document.querySelector('.search-form');
+// const searchBtn = document.querySelector('.search-btn');
+// const searchForm = document.querySelector('.search-form');
 
-searchBtn.addEventListener('click', () => {
-  searchForm.classList.toggle('d-none');
-});
+// searchBtn.addEventListener('click', () => {
+//   searchForm.classList.toggle('d-none');
+// });
 
-// Initialize the carousel
-var myCarousel = document.querySelector('#carouselExampleIndicators')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 5000, // Change interval time between each slide
-  wrap: true, // Set to false to stop automatic looping
-  keyboard: true, // Set to false to disable keyboard navigation
-  pause: 'hover' // Set to false to disable pause on hover
-})
+// // Initialize the carousel
+// var myCarousel = document.querySelector('#carouselExampleIndicators')
+// var carousel = new bootstrap.Carousel(myCarousel, {
+//   interval: 5000, // Change interval time between each slide
+//   wrap: true, // Set to false to stop automatic looping
+//   keyboard: true, // Set to false to disable keyboard navigation
+//   pause: 'hover' // Set to false to disable pause on hover
+// })
--- assets/js/main.js
+++ assets/js/main.js
@@ -105,43 +105,7 @@
     onscroll(document, toggleBacktotop)
   }
 
-  /**
-   * Mobile nav toggle
-   */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
-
-  /**
-   * Mobile nav dropdowns activate
-   */
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
-
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
-
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
-      scrollto(this.hash)
-    }
-  }, true)
-
+  
   /**
    * Scroll with ofset on page load with hash links in the url
    */
@@ -348,11 +312,6 @@ $(".mainmenu-area").sticky({topSpacing:0});
       }
   });    
   
-  
-  // Bootstrap Mobile Menu fix
-  $(".navbar-nav li a").click(function(){
-      $(".navbar-collapse").removeClass('in');
-  });    
   
   // jQuery Scroll effect
   $('.navbar-nav li a, .scroll-to-up').bind('click', function(event) {
